ecaa9c42fa254a652c51ce404c4bbd9fe7250a72
bli013 <btli@ymail.com>
2020-04-07 15:46:52 -0700

    rerouted the root page to be the sign in page and can now sign in and sign out

---
+++ app/javascript/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react"
+class HomePage extends React.Component {
+    render () {
+  
+  
+      return (
+        <React.Fragment>
+       <h1>I am the Home Page Component</h1>
+        </React.Fragment>
+      );
+    
+  }
+  
+export default HomePage
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :tickets
-  devise_for :users
-  get '*path', to: 'index#root', constraints: ->(request){ request.format.html? }
-  root to: "index#index"
+  devise_for :users, skip: [:sessions]
+  as :user do
+    get '/', to: 'devise/sessions#new', as: :new_user_session
+    post 'sign_in_route', to: 'devise/sessions#create', as: :user_session
+    get 'sign_out_route', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 end
+
+

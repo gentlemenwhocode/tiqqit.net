83d9c6ba67693429d91b9ef8b791f77a5a55766e
RudyBecker <52896529+RudyBecker@users.noreply.github.com>
2020-04-13 14:41:48 -0700

    edit TicketEdit.js Rnd 2

--- app/javascript/components/pages/TicketEdit.js
+++ app/javascript/components/pages/TicketEdit.js
@@ -104,21 +104,34 @@ class TicketEdit extends Component {
                           disabled={!ready}
                           onChange={e => handleUpload(e.currentTarget.files)}
                         />
-                        
+
+                        {uploads.map(upload => {
+                          switch (upload.state) {
+                            case 'waiting':
+                              return <p key={upload.id}>Waiting to upload {upload.file.name}</p>
+                            case 'uploading':
+                              return (
+                                <p key={upload.id}>
+                                  Uploading {upload.file.name}: {upload.progress}%
+                                </p>
+                              )
+                            case 'error':
+                              return (
+                                <p key={upload.id}>
+                                  Error uploading {upload.file.name}: {upload.error}
+                                </p>
+                              )  
+                            case 'finished':
+                              return (
+                                <p key={upload.id}>Finished uploading {upload.file.name}</p>
+                              )
+                          }
+                        })}
                     )}
                   </div>
-                }
-
-
-
-
-
-
-
-
-
-
-
+                    )}
+                    />
+                  </div>
 
 
                 <table className="table table-hover">
@@ -212,8 +225,10 @@ class TicketEdit extends Component {
                   </tbody>
                 </table>
                 { this.state.success && <Redirect to="/ticketindex"/> }
+                
+          
               </React.Fragment>
-            );
+          );
           }
         }
 

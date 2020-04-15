c77ee8e40dc09172d68bbd66cb71646dd3785c8d
RudyBecker <52896529+RudyBecker@users.noreply.github.com>
2020-04-15 09:35:05 -0700

    delete migration for :photo column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_230441) do
+ActiveRecord::Schema.define(version: 2020_04_13_233158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

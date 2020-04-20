bb7f5e10d8d4179b1199afeacfd23c2928c61299
learn <learn@LEARN-16.local>
2020-04-20 15:33:01 -0700

    add gihpy api to randomly display a fail gif. Refactor code to . notation of grabbing values out of the json object.

--- app/javascript/components/pages/NotFoundPage.js
+++ app/javascript/components/pages/NotFoundPage.js
@@ -10,19 +10,19 @@ class NotFoundPage extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            gifs: [],
+            gifs: null,
             isLoaded: false
         }
         this.getGifs()
     }
-
+    //React lifecyle check, if the component mounted, run the get.Gifs method. 
         componentDidMount() {
             this.getGifs()
         }
 
-
+    //This will do a fetch request to the Giphy API, if the response is OK it will then setstate of Gif's to a json object with the key of data.
     getGifs = () => {
-        fetch(`https://api.giphy.com/v1/gifs/search?q=fail&api_key=zpIByyzSBrKBLfSgwgwdfzqhYRoqluQn&limit=1`)
+        fetch(`https://api.giphy.com/v1/gifs/random?api_key=zpIByyzSBrKBLfSgwgwdfzqhYRoqluQn&tag=fail&rating=PG-13`)
        .then((response)=>{
            if(response.status === 200) {
                return(response.json())
@@ -35,32 +35,24 @@ class NotFoundPage extends React.Component {
            })
             })
   }
-
+    // This lil bad-boy right here checks to see if the state has been set by the fetch call has changed the state yet. if not, show a loading bar. If not, go ahead and render the rest of the page.
     render(){
         var { isLoaded, gifs } = this.state;
         
         if(!isLoaded) {
             return <div> LOADING...</div>
         } else {
-            console.log(this.state.gifs)
-
-
         return (
         <React.Fragment>
-       
-            { this.state.gifs.map((data, index) => {
-                return (
-                    <Card style={{marginLeft:"65px", marginTop:"50px", maxWidth:"400px"}} key={ index }>
+                    <Card style={{marginLeft:"65px", marginTop:"50px", maxWidth:"400px"}}>
                         <CardHeader> Oof, thats a 404! </CardHeader>
-                        <CardImg style={{maxWidth: "400px"}} src={ data.images.downsized_large.url } alt="404 error gif"/>
+                        <CardImg style={{maxWidth: "400px"}} src={ this.state.gifs.images.downsized_large.url } alt="404 error gif"/>
                         <CardBody>
                             <CardText> Looks like you tried to go somewhere that doesnt exist!</CardText>
                             <CardText><Link to="/"> Go back to the home page</Link></CardText>
                         </CardBody>
                     </Card>
-                )
-              }
-            )}
+                
         </React.Fragment>
         )}
     }
